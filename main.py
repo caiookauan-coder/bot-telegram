a3ec96a65436b902aaf27d6b9a370504eb04b020
caiookauan-coder <caiookauan@gmail.com>
2026-06-08 14:45:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ TOKEN = os.getenv("8037756153:AAHC3HRsY1TEDtWW9ZtGRKETkbCYv79Yr1A"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda m: True)
-def responder(message):
+@bot.message_handler(func=lambdam: True)
+defresponder(message):
     bot.reply_to(message, "Olá! Estou funcionando.")
 
 bot.infinity_polling()
